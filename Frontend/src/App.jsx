4a1fe1831928e08381e7c082b2ae5f30b445f2e5
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2025-04-26 23:30:43 +0530

    add page and set it routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Post_add from './pages/Post_add';
 import Post_views from './pages/Post_views';
 import Learning_Home from './pages/Learning_Home';
 import Learning_add from './pages/Learning_add';
+import Learnig_share_recipe from './pages/Learnig_share_recipe';
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
         <Route path="/Post_views" element={<Post_views />} />
         <Route path="/Learning_Home" element={<Learning_Home />} />
         <Route path="/Learning_add" element={<Learning_add />} />
+        <Route path="/Learnig_share_recipe" element={<Learnig_share_recipe />} />
         
       </Routes>
     </BrowserRouter>
