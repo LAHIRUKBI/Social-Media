190d58c7d80210b593fb90c1da29e222728822b9
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2025-05-17 15:10:27 +0530

    adds

--- Frontend/src/Components/Navigation.jsx
+++ Frontend/src/Components/Navigation.jsx
@@ -30,7 +30,7 @@ export default function Navigation() {
 
         {/* Logo */}
         <div className="text-3xl font-black tracking-wide text-red-600 flex items-center gap-1">
-          Cook<span className="text-gray-800">Book</span>
+          Food<span className="text-gray-800">Gram</span>
         </div>
 
         {/* Navigation Links */}
